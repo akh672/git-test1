e712bc94b484fab3b4930aaf74f2b92d63715123
akh672 <121810313059@gitam.edu>
2023-10-13 23:12:16 +0530

    My First Commit

---
+++ test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println(293* 12);
+         System.out.println(982 * 121);
+          System.out.println(38 * 7);
+    }
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log(38 * 82);
+console.log(72 * 12);
+console.log(73 * 72);
